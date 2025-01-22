57b768825759c55b934704627ea04c12c1c5ca78
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2025-01-22 09:26:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,73 +1,60 @@
 const express = require('express');
-const {Pool} = require('pg');
+const sequelize = require('./sequelize');
+const Todo = require('./Models/Todo');
 
-const app=express();
-const port=4000;
+const app = express();
+const port = 4000;
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'todos',
-    password: 'root',
-    port: 5432,
-});
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully');
+        return sequelize.sync({ alter: true});
+    })
+    .then(() => {
+        console.log("All models were Synchronized successfully.");
+    })
+    .catch(() => {
+        console.error('Unable to connect to the database:', error);
+    });
 
 app.use(express.json());
 
-//Get all todos
-app.get('/todos', (req, res) => {
-    pool.query('SELECT * FROM todos', (error, result) => {
-        if (error){
-            console.error('Error fectching todos', error);
-            res.status(500).json({ error: 'Internal server error'});
-        }else{
-            res.json(result.rows);
-        }
-    });
-});
+app.get('/todos', (req , res) => {
+    Todo.findAll()
+        .then((todos) =>{
+            res.json(todos);
+        })
+        .catch((error) => {
+            res.status(500).json({error: 'Internal'})
+        });
 
-app.post('/todos', (req,res)=>{
+});
+app.put("/todos/:id", (req, res) => {
+    const todoId = req.params.id;
+    const { title, completed } = req.body;
+    Todo.findByPk(todoId)
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).json({ error: "Todo not found" });
+        }
+        todo.title = title;
+        todo.completed = completed;
+        return todo.save();
+      })
+      .then((updatedTodo) => {
+        res.json(500).json({ error: "Internal server error" });
+     });
+});
+app.post('/todos',(req , res) => {
     const { title, completed} = req.body;
-    pool.query('INSERT INTO todos (title, completed) VALUES ($1,$2)', [title, completed], (error) => {
-        if(error){
-            console.error('Error creating todo',error);
-            res.status(500).json({error:'Internal server error'});
-        }else{
-            res.status(201).json({message: "Todo created successfully"});
-        }
+    Todo.create({title, completed})
+    .then((todo) =>{
+        res.status(201).json(todo);
+    })
+    .catch((error) => {
+        res.status(400).json({error: 'Bad request'});
     });
-
 });
-
-//Put update todos
-app.put('/todos/:id', (req, res) =>{
-    const { id} =req.params;
-    const {title, completed} = req.body;
-    pool.query('UPDATE todos SET title = $1, completed = $2 WHERE id=$3', [title, completed, id], (error) => {
-        if(error){
-            console.error('Error updating todo',error);
-            res.status(500).json({error:'Internal server error'});
-        }else{
-            res.json({message: "Todos updated successfully"});
-        }
-    });
-})
-
-app.delete('/todos/:id', (req,res)=>{
-    const {id} = req.params;
-    pool.query('DELETE FROM todos WHERE title = $1',[id], (error) => {
-        if(error){
-            console.error('Error creating todo',error);
-            res.status(500).json({error:'Internal server error'});
-        }else{
-            res.json({message: "Todo deleted successfully"});
-        }
-    });
-
+app.listen(port, ()=>{
+    console.log("server is live");
 });
-
-app.listen(4000, ()=>{
-    console.log('Server is running.');
-});
-
-                                                
