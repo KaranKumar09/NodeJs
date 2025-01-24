0c493625cb4bb91be1aaadbe6879029fa63461b7
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2025-01-24 10:23:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,78 @@
 const express = require('express');
-const sequelize = require('./sequelize');
-const Todo = require('./Models/Todo');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 4000;
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully');
-        return sequelize.sync({ alter: true});
-    })
-    .then(() => {
-        console.log("All models were Synchronized successfully.");
-    })
-    .catch(() => {
-        console.error('Unable to connect to the database:', error);
-    });
-
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/todos', (req , res) => {
-    Todo.findAll()
-        .then((todos) =>{
-            res.json(todos);
-        })
-        .catch((error) => {
-            res.status(500).json({error: 'Internal'})
-        });
+const mongoURI = process.env.MONGO_URI;
 
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Error connecting to MongodB", err));
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
 });
-app.put("/todos/:id", (req, res) => {
-    const todoId = req.params.id;
-    const { title, completed } = req.body;
-    Todo.findByPk(todoId)
-      .then((todo) => {
-        if (!todo) {
-          return res.status(404).json({ error: "Todo not found" });
-        }
-        todo.title = title;
-        todo.completed = completed;
-        return todo.save();
-      })
-      .then((updatedTodo) => {
-        res.json(500).json({ error: "Internal server error" });
-     });
+
+const User = mongoose.model('user',userSchema);
+
+app.get('/users',(req,res) => {
+    User.find({})
+        .then(users => res.json(users))
+        .catch(err => res.status(500).json({message:err.message}));
 });
-app.post('/todos',(req , res) => {
-    const { title, completed} = req.body;
-    Todo.create({title, completed})
-    .then((todo) =>{
-        res.status(201).json(todo);
-    })
-    .catch((error) => {
-        res.status(400).json({error: 'Bad request'});
+
+
+app.post('/users',(req,res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     });
+    user.save()
+        .then(newUser => res.status(201).json(newUser))
+        .catch(err => res.status(400).json({message: err.message}));
 });
-app.listen(port, ()=>{
-    console.log("server is live");
+
+app.put('/users/:id',(req,res) => {
+    const userID = req.params.id;
+    const updateData = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    User.findByIdAndUpdate(userID,updateData,{new : true})
+        .then(updatedUser => {
+            if(!updatedUser) {
+                return res.status(404).json({message: 'User not found'});
+            }
+            res.json(updatedUser);
+        })
+        .catch(err => res.status(400).json({message: err.message}));
 });
+
+
+app.delete("/users/:id", (req,res)=>{
+    const userID = req.params.id;
+
+    User.findByIdAndDelete(userID)
+        .then(deleteUser => {
+            if(!deleteUser) {
+                return req.status(404).json({message: 'User not found'});
+            }
+            res.json({message: 'User deleted successfully'});
+        })
+        .catch(err => res.status(400).json({message: err.message}));
+});
+
+
+app.listen(port, ()=>console.log('Server is live'));
