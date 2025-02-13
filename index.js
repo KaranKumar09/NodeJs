a0881ce498f139b85c99c3c00203569141c5fe3b
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2025-02-13 10:27:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
-const express = require('express');
-const sequelize = require('./sequelize');
-const Todo = require('./Models/Todo');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
 const app = express();
-const port = 4000;
+const server = app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully');
-        return sequelize.sync({ alter: true});
-    })
-    .then(() => {
-        console.log("All models were Synchronized successfully.");
-    })
-    .catch(() => {
-        console.error('Unable to connect to the database:', error);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from 'public' directory
+
+const connectedUsers = {}; // Store connected users and their usernames
+
+io.on("connection", (socket) => {
+  console.log("A user connected to the server");
+
+  socket.on("join", (username) => {
+    connectedUsers[socket.id] = username;
+    socket.username = username;
+    io.emit("chat message", {
+      type: "notification",
+      message: `${username} has joined the chat`,
     });
+  });
 
-app.use(express.json());
-
-app.get('/todos', (req , res) => {
-    Todo.findAll()
-        .then((todos) =>{
-            res.json(todos);
-        })
-        .catch((error) => {
-            res.status(500).json({error: 'Internal'})
-        });
-
-});
-app.put("/todos/:id", (req, res) => {
-    const todoId = req.params.id;
-    const { title, completed } = req.body;
-    Todo.findByPk(todoId)
-      .then((todo) => {
-        if (!todo) {
-          return res.status(404).json({ error: "Todo not found" });
-        }
-        todo.title = title;
-        todo.completed = completed;
-        return todo.save();
-      })
-      .then((updatedTodo) => {
-        res.json(500).json({ error: "Internal server error" });
-     });
-});
-app.post('/todos',(req , res) => {
-    const { title, completed} = req.body;
-    Todo.create({title, completed})
-    .then((todo) =>{
-        res.status(201).json(todo);
-    })
-    .catch((error) => {
-        res.status(400).json({error: 'Bad request'});
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", {
+      type: "message",
+      username: socket.username,
+      message: msg,
     });
-});
-app.listen(port, ()=>{
-    console.log("server is live");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    if (connectedUsers[socket.id]) {
+      const username = connectedUsers[socket.id];
+      delete connectedUsers[socket.id];
+      io.emit("chat message", {
+        type: "notification",
+        message: `${username} has left the chat`,
+      });
+    }
+  });
 });
